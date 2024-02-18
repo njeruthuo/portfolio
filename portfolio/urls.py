be9ff59922fbeba8b42fc4167326a888fc3671cb
njeruthuo <njeruthelearner@gmail.com>
2024-02-19 02:35:49 +0300

    Frontend added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("projects/", include("projects.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
